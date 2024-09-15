8200573e8b269968f5e4726763c38f2e18f3292f
Adam Sabbah <Adam Sabbah>
2024-09-16 02:25:15 +0300

    updates

--- lang/en/banner.php
+++ lang/en/banner.php
@@ -4,4 +4,5 @@ return [
     'title' => 'Pure Beauty, Naturally: Dead Sea Levant',
     'description' => 'At Dead Sea Levant, we believe in the power of nature to enhance your beauty and well-being. Our products are crafted with natural minerals from the Dead Sea, free from harmful artificial additives. Experience the pure, rejuvenating benefits of our all-natural skincare range, designed to nurture your skin and elevate your natural beauty. Trust in nature, trust in Dead Sea Levant.',
     'button' => 'Shop all products',
+    'products_title' => 'Explore Our Most Selling Products',
 ];
