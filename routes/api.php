6b6439cfda19d9222d31505e1a92006ba829537b
Adam Sabbah <Adam Sabbah>
2024-09-16 03:56:24 +0300

    updates

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\LanguageController;
 use Illuminate\Support\Facades\Route;
 
 
-// Route::get('languages', [LanguageController::class, 'index']);
-// require 'api/home.php';
-// require 'api/customer.php';
-// require 'api/cart.php';
+Route::get('languages', [LanguageController::class, 'index']);
+require 'api/home.php';
+require 'api/customer.php';
+require 'api/cart.php';
