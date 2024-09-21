487a5f65d8b75763b8ab1c4beeca248b17eb174c
Adam Sabbah <Adam Sabbah>
2024-09-21 13:12:01 +0300

    updates

--- lang/en/navbar.php
+++ lang/en/navbar.php
@@ -11,6 +11,6 @@ return [
     'logged_in_item3' => 'Payment method',
     'logged_in_item4' => 'Sign out',
     'not_logged_in_item1' => 'Log in',
-    'not_logged_in_item2' => 'Crate account',
+    'not_logged_in_item2' => 'Create account',
     'search' => 'Search for products or information',
 ];
