4fbd75834fe6f7750bbd9b1b182ce8f773ba1707
Adam Sabbah <Adam Sabbah>
2024-10-04 14:06:45 +0300

    updates

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,28 +1,27 @@
 {{-- This file is used for menu items by any Backpack v6 theme --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
-<x-backpack::menu-dropdown title="Add-ons" icon="la la-puzzle-piece">
-    <x-backpack::menu-dropdown-header title="Authentication" />
-    <x-backpack::menu-dropdown-item title="Users" icon="la la-user" :link="backpack_url('user')" />
-    <x-backpack::menu-dropdown-item title="Roles" icon="la la-group" :link="backpack_url('role')" />
-    <x-backpack::menu-dropdown-item title="Permissions" icon="la la-key" :link="backpack_url('permission')" />
+<x-backpack::menu-dropdown title="Products" icon="la la-box-open">
+    <x-backpack::menu-item title="Categories" icon="la la-tags" :link="backpack_url('category')" />
+    <x-backpack::menu-item title="Products" icon="la la-box-open" :link="backpack_url('product')" />
+    <x-backpack::menu-item title="Product images" icon="la la-photo-video" :link="backpack_url('product-image')" />
 </x-backpack::menu-dropdown>
+
+<x-backpack::menu-item title="Emails" icon="la la-mail-bulk" :link="backpack_url('email')" />
+<x-backpack::menu-item title="Orders" icon="la la-shopping-cart" :link="backpack_url('order')" />
+<x-backpack::menu-item title="Contact requests" icon="la la-comment-dots" :link="backpack_url('contact-request')" />
+
+<x-backpack::menu-dropdown-item title="Users" icon="la la-user" :link="backpack_url('user')" />
 
 <x-backpack::menu-dropdown title="Translations" icon="la la-globe">
     <x-backpack::menu-dropdown-item title="Languages" icon="la la-flag-checkered" :link="backpack_url('language')" />
     <x-backpack::menu-dropdown-item title="Site texts" icon="la la-language" :link="backpack_url('language/texts')" />
 </x-backpack::menu-dropdown>
 
-<x-backpack::menu-item title="Settings" icon="la la-question" :link="backpack_url('setting')" />
-<x-backpack::menu-item title='Logs' icon='la la-terminal' :link="backpack_url('log')" />
-<x-backpack::menu-item title="images" icon="la la-question" :link="backpack_url('images')" />
-<x-backpack::menu-item title="Categories" icon="la la-question" :link="backpack_url('category')" />
-<x-backpack::menu-item title="Countries" icon="la la-question" :link="backpack_url('country')" />
-<x-backpack::menu-item title="Order statuses" icon="la la-question" :link="backpack_url('order-status')" />
-<x-backpack::menu-item title="Products" icon="la la-question" :link="backpack_url('product')" />
-<x-backpack::menu-item title="Product images" icon="la la-question" :link="backpack_url('product-image')" />
-<x-backpack::menu-item title="Contact requests" icon="la la-question" :link="backpack_url('contact-request')" />
-
-<x-backpack::menu-item title="Emails" icon="la la-question" :link="backpack_url('email')" />
-
-<x-backpack::menu-item title="Orders" icon="la la-question" :link="backpack_url('order')" />
+<x-backpack::menu-dropdown title="Settings" icon="la la-cog">
+    <x-backpack::menu-item title="Settings" icon="la la-cogs" :link="backpack_url('setting')" />
+    <x-backpack::menu-item title='Logs' icon='la la-terminal' :link="backpack_url('log')" />
+    <x-backpack::menu-item title="images" icon="la la-image" :link="backpack_url('images')" />
+    <x-backpack::menu-item title="Countries" icon="la la-globe" :link="backpack_url('country')" />
+    <x-backpack::menu-item title="Order statuses" icon="la la-stream" :link="backpack_url('order-status')" />
+</x-backpack::menu-dropdown>
