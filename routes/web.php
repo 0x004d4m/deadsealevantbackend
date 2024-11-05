0902ebbdf65656c952401ff3d60174b13a28a50d
Adam Sabbah <Adam Sabbah>
2024-11-05 16:16:05 +0300

    updates

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     // Notification::route('telegram', env('TELEGRAM_ADMIN_CHAT_ID'))
     // ->notify(new TestNotification()); Bb197310!@#
-    // return view('welcome');
-    return view('mail.order', [
-        'Order' => Order::get()->last()
-    ]);
+    return view('welcome');
+    // return view('mail.order', [
+    //     'Order' => Order::get()->last()
+    // ]);
 });
